172afbd5d6ceb3a70919a49f2e4da05b2cbc2410
mkwon-hsct <mkwon.hsct@gmail.com>
2021-09-04 00:19:23 +0900

    Added list constructor and join functions.

--- chapter_7/shared_library/src/manage.c
+++ chapter_7/shared_library/src/manage.c
@@ -71,7 +71,7 @@ K nullify(){
  */
 K pure_waste(){
   K time = kt(60*60*1000);
-  // Change the mind
+  // Changed the mind
   r0(time);
   return (K) 0;
 }
@@ -83,6 +83,24 @@ K complex_waste(K garbage){
   K new_garbage=garbage;
   new_garbage->i += 1;
   return (K) 0;
+}
+
+/**
+ * @brief Create a string.
+ * @return 
+ * - string
+ */
+K create_string(){
+  return kp("this is a string.");
+}
+
+/**
+ * @brief Create a string with the first 14 characters.
+ * @return 
+ * - string
+ */
+K create_string2(){
+  return kpn("cannot see whole message!", 14);
 }
 
 /**
@@ -166,6 +184,8 @@ K compound_list2(){
 
 /**
  * @brief Create a dictionary.
+ * @return 
+ * - dictionary
  */
 K create_dictionary(){
 
@@ -174,10 +194,7 @@ K create_dictionary(){
   kS(keys)[1]=ss("longs");
   kS(keys)[2]=ss("seconds");
 
-  K values = ktn(0, 3);
-  K value1 = ktn(KC, 2);
-  kC(value1)[0]='a';
-  kC(value1)[1]='b';
+  K value1 = kp("ab");
 
   K value2 = ktn(KJ, 2);
   kJ(value2)[0]=1000000LL;
@@ -187,15 +204,161 @@ K create_dictionary(){
   kI(value3)[0]=210;
   kI(value3)[1]=wi;
 
-  K values_[3]= {value1, value2, value3};
-  memcpy((void*) kK(values), (const void*) values_, 3 * sizeof(K));
+  // Create a compound list from three lists
+  K values = knk(3, value1, value2, value3);
 
   return xD(keys, values);
 }
 
 /**
  * @brief Create a table from a dictionary.
+ * @return 
+ * - table
  */
 K create_table(){
   return xT(create_dictionary());
+}
+
+/**
+ * @brief Convert a table into a keyed table.
+ * @return 
+ * - keyed table
+ */
+K enkey(){
+  // Hold as an object
+  K table = create_table();
+  K keyed_table = knt(1, table);
+  // Free the table
+  r0(table);
+  return keyed_table;
+}
+
+/**
+ * @brief Convert a keyed table into a table.
+ * @return 
+ * - keyed table
+ */
+K unkey(){
+  K table = enkey();
+  return ktd(table);
+}
+
+/**
+ * @brief Build a keyed table from two tables.
+ * @return 
+ * - keyed table
+ */
+K artificial_keyed_table(){
+
+  // Table1
+  K keys1 = ktn(KS, 1);
+  kS(keys1)[0]=ss("chars");
+
+  K value1_1 = ktn(KC, 2);
+  strcpy(kC(value1_1), "ab");
+
+  // Everything is moved!!
+  K key_table = xT(xD(keys1, knk(1, value1_1)));
+
+  // Table2
+  K keys2 = ktn(KS, 2);
+  kS(keys2)[0]=ss("longs");
+  kS(keys2)[1]=ss("seconds");
+
+  K value2_1 = ktn(KJ, 2);
+  kJ(value2_1)[0]=1000000LL;
+  kJ(value2_1)[1]=2000000LL;
+
+  K value2_2 = ktn(KV, 2);
+  kI(value2_2)[0]=210;
+  kI(value2_2)[1]=wi;
+
+  // Everything is moved!!
+  K value_table = xT(xD(keys2, knk(2, value2_1, value2_2)));
+  
+  return xD(key_table, value_table);
+}
+
+/**
+ * @brief Create a compound list by adding elements.
+ * @return 
+ * - compound list
+ */
+K add_to_compound(){
+  K list = ktn(0, 0);
+  jk(&list, ks("1st"));
+  for(int i = 1; i!= 4; ++i){
+    jk(&list, ki(i));
+  }
+  return list;
+}
+
+/**
+ * @brief Create a symbol list by adding elemens.
+ * @return
+ * - symbol list
+ */
+K add_to_symbol_list(){
+  K list = ktn(KS, 0);
+  js(&list, ss("majesty"));
+  js(&list, ss("glory"));
+  return list;
+}
+
+/**
+ * @brief Create a simple list by adding elements.
+ * @return 
+ * - list of time
+ */
+K add_to_simple(){
+  K list = ktn(KT, 2);
+  kI(list)[0] = 15;
+  kI(list)[1] = 30;
+  I forty_five = 45;
+  I sixty = 60;
+  ja(&list, (void *) &forty_five);
+  ja(&list, (void *) &sixty);
+  forty_five*=4;
+  ja(&list, (void *) &forty_five);
+  return list;
+}
+
+/**
+ * @brief Concatenate two same type of lists.
+ * @param mode {bool}: Flag to switch result type:
+ * - true: Return compound list.
+ * - false: Return simple list.
+ * @return
+ * - compound list: If `mode` is true.
+ * - simple list: If `mode` is false.
+ */
+K concat_list(K mode){
+  if(mode->t != -KB){
+    return krr("mode must be bool type");
+  }
+  switch(mode->g){
+    case 1:
+      {
+        K list1 = knk(3, kg(0x4d), ke(1.23), ktj(-KP, 130980641756951));
+        K list2 = knk(2, kh(-8), kc('k'));
+        jv(&list1, list2);
+        // Free unnecessary list
+        r0(list2);
+        return list1;
+      }
+    default:
+      {
+        K list1 = ktn(KF, 2);
+        kF(list1)[0]=15.982;
+        kF(list1)[1]=3.16;
+
+        K list2 = ktn(KF, 2);
+        kF(list2)[0]=100.0315;
+        kF(list2)[1]=-0.349;
+        jv(&list1, list2);
+        // Free unnecessary list
+        r0(list2);
+        return list1;
+      }
+  }
 }
